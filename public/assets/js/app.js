8d3142cd83cc540f70a3a126f0a4d47896c6352d
guillecro <g.croppi@hotmail.com>
2014-10-07 01:08:17 -0300

    Se añadieron los regex de campos nombre y apellido p/ registro. Se cambio un poco unos layout.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+    abide : {
+      patterns: {
+        nombre: /(^([A-Za-zñÑáéíóúÁÉÍÓÚ]+)$)|(^([A-Za-zñÑáéíóúÁÉÍÓÚ]+)\s([A-Za-zñÑáéíóúÁÉÍÓÚ]+)$)/,
+		apellido: /(^([A-Za-zñÑáéíóúÁÉÍÓÚ]+)$)/,
+		//Email Validation as per RFC2822 standards.
+		email: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+      }
+    }
+  });
 $(document).ready(function(){$('#modalErrors').foundation('reveal', 'open')});
+
