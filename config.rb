4131ff3c2b7f65536d37932d454d273c7f779894
guillecro <g.croppi@hotmail.com>
2015-03-26 07:27:39 -0300

    Fix de CSS y algunas cuestiones relacionadas con compass

--- config.rb
+++ config.rb
@@ -5,14 +5,15 @@ add_import_path "bower_components/foundation/scss"
 http_path = "/"
 css_dir = "public/assets/css"
 sass_dir = "public/assets/scss"
-images_dir = "public/assets/images"
+images_dir = "public/assets/img"
 javascripts_dir = "public/assets/js"
+fonts_dir = "public/assets/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
