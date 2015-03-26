a60689614d9802d30ec0979c00a7799b2fe72655
guillecro <g.croppi@hotmail.com>
2015-03-26 07:20:36 -0300

    Rediseño de Virtuágora.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,17 +1,3 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation({
-    abide : {
-        patterns: {
-            nombre: /(^([A-Za-zñÑáéíóúÁÉÍÓÚ]+)$)|(^([A-Za-zñÑáéíóúÁÉÍÓÚ]+)\s([A-Za-zñÑáéíóúÁÉÍÓÚ]+)$)/,
-            apellido: /(^([A-Za-zñÑáéíóúÁÉÍÓÚ]+)$)/,
-            //Email Validation as per RFC2822 standards.
-            email: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-        }
-    }
-});
-
-$(document).ready(function ( ) {
-    $('#modalErrors').foundation('reveal', 'open');
-});
-
+$(document).foundation();
