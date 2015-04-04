59b169262bbb78c8063114dabcf7b4a18df1b0f3
guillecro <g.croppi@hotmail.com>
2015-04-04 12:59:32 -0300

    Fix de las vistas de crear, modificar y ver novedad al igual que update del estilo.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$( ".bt-logout" ).click(function() {
+        $('#form-salir').submit();
+    });
