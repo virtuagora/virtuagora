03436bd7ba285228af38cd2f5bc4c0e0b1ae48fe
guillecro <g.croppi@hotmail.com>
2015-04-10 13:52:03 -0300

    Realizado el paginator para los organismos y verificar ciudadanos.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,5 +3,27 @@
 $(document).foundation();
 
 $( ".bt-logout" ).click(function() {
-        $('#form-salir').submit();
+    $('#form-salir').submit();
+});
+
+function parse_link_header(header) {
+    if (header.length == 0) {
+        throw new Error("input must not be of zero length");
+    }
+
+    // Split parts by comma
+    var parts = header.split(', ');
+    var links = {};
+    // Parse each part into a named link
+    _.each(parts, function(p) {
+        var section = p.split(';');
+        if (section.length != 2) {
+            throw new Error("section could not be split on ';'");
+        }
+        var url = section[0].replace(/<(.*)>/, '$1').trim();
+        var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+        links[name] = url;
     });
+
+    return links;
+}
